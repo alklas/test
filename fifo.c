2bbb10007498f93e900aca9d20a06f9b7a6b1050
A.B. Klassov <alklas@mail.ru>
2024-05-07 12:20:29 +0700

    Изменили работу FIFO для обмена между неродственными процессами

--- fifo.c
+++ fifo.c
@@ -6,9 +6,8 @@
 #include <stdlib.h>
 
 int main() {
-	int fd, result;
+	int fd;
 	size_t size;
-	char resstring[13];
 	//Создаём сивольный массив, где будет храниться имя файла FIFO
 	char name[] = "fifo.fifo";
 	//После первого запуска программы необходимо закомментировать процесс создания файла FIFO
@@ -17,44 +16,19 @@ int main() {
 		exit(-1);
 	}
 */
-	//Порождаем дочерний процесс
-	if ((result = fork()) < 0) {
-		printf("Не удалось создать дочерний процесс\n");
+	if((fd = open(name, O_WRONLY)) < 0) {
+		printf("Не удалось открыть файл на запись\n");
 		exit(-1);
 	}
-	//Процесс-родитель пишет информацию в FIFO
-	else if (result > 0) {
-		if((fd = open(name, O_WRONLY)) < 0) {
-			printf("Не удалось открыть файл на запись\n");
-			exit(-1);
-		}
-		size = write(fd, "Hello, child!", 13);
-		if (size != 13) {
-			printf("Не получилось записать 13 байт в FIFO\n");
-			exit(-1);
-		}
-		if (close(fd)<0){
-			printf("Не получилось закрыть FIFO, открытый за запись\n");
-			exit(-1);
-		}
-		printf("Процесс-родитель записал информацию в FIFO и завершил работу\n");
+	size = write(fd, "Hello, all!", 11);
+	if (size != 11) {
+		printf("Не получилось записать 11 байт в FIFO\n");
+		exit(-1);
 	}
-	// Процесс-ребёнок читает информацию из FIFO
-	else {
-		if ((fd = open(name, O_RDONLY)) < 0) {
-			printf("Не удалось открыть FIFO для чтения\n");
-			exit(-1);
-		}
-		size = read(fd, resstring, 13);
-		if (size != 13) {
-			printf("Не удалось прочитать информацию из FIFO\n");
-			exit(-1);
-		}
-		printf("Процесс-ребёнок прочитал информацию: %s\n", resstring);
-		if (close(fd)<0) {
-			printf("Не получилось закрыть FIFO, открытый на чтение\n");
-			exit(-1);
-		}
+	if (close(fd)<0){
+		printf("Не получилось закрыть FIFO, открытый за запись\n");
+		exit(-1);
 	}
+	printf("Процесс fifo записал информацию в FIFO и завершил работу\n");
 	return 0;
 }
